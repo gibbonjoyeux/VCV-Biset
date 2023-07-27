2be4fb92e219eedfd484e8cbd03bdb5285cd7593
gibbonjoyeux <sebastien.huertas@protonmail.com>
2023-07-27 21:12:05 +0200

    [~]Tracker: got wip CV column visual curve

--- src/Tracker/TrackerDisplay-draw_pattern.cpp
+++ src/Tracker/TrackerDisplay-draw_pattern.cpp
@@ -39,6 +39,320 @@ static void ptext(const Widget::DrawArgs& args, Vec p, float x, float y,
 	nvgText(args.vg, sx, sy, str, NULL);
 }
 
+//void TrackerDisplay::draw_pattern_visual(const DrawArgs &args, Rect rect) {
+//	PatternSource			*pattern;
+//	PatternNote				*note;
+//	PatternNoteCol			*note_col;
+//	//PatternCV				*cv;
+//	PatternCVCol			*cv_col;
+//	PatternEffect			*effect;
+//	Vec						p;
+//	int						i, j, k;
+//	int						line;
+//	//float					x, y;
+//	int						tx, tx_col;
+//	//char					str[32];
+//
+//	/// [2] LAYER 2 (TRACKER)
+//	/// FOR EACH NOTE COL	
+//	tx = 0;
+//	for (i = 0; i < pattern->note_count; ++i) {
+//		note_col = pattern->notes[i];
+//		//focus_col = (g_editor->pattern_col == i);
+//		/// FOR EACH NOTE COL LINE
+//		for (j = 0; j < CHAR_COUNT_Y; ++j) {
+//			line = g_editor->pattern_cam_y + j;
+//			if (line >= pattern->line_count)
+//				break;
+//			//focus_line = focus_col & (g_editor->pattern_line == line);
+//			tx_col = tx;
+//			note = &(note_col->lines[line]);
+//			/// PITCH
+//			//focus = focus_line & (g_editor->pattern_cell == 0);
+//			//nvgFillColor(args.vg, colors[3]);
+//			//if (note->mode == PATTERN_NOTE_KEEP) {
+//			//	str[0] = '.';
+//			//	str[1] = '.';
+//			//	str[2] = 0;
+//			//	ptext(args, p, tx_col, j, str, 3, focus);
+//			//} else if (note->mode == PATTERN_NOTE_STOP) {
+//			//	str[0] = '-';
+//			//	str[1] = '-';
+//			//	str[2] = 0;
+//			//	ptext(args, p, tx_col, j, str, 3, focus);
+//			//} else {
+//			//	ptext(args, p, tx_col, j,
+//			//	/**/ table_pitch[note->pitch % 12], 3, focus);
+//			//}
+//			tx_col += 2;
+//			/// OCTAVE
+//			//focus = focus_line & (g_editor->pattern_cell == 1);
+//			//nvgFillColor(args.vg, colors[2]);
+//			//if (note->mode == PATTERN_NOTE_KEEP) {
+//			//	str[0] = '.';
+//			//	str[1] = 0;
+//			//} else if (note->mode == PATTERN_NOTE_STOP) {
+//			//	str[0] = '-';
+//			//	str[1] = 0;
+//			//} else {
+//			//	itoa(note->pitch / 12, str, 10);
+//			//}
+//			//ptext(args, p, tx_col, j, str, 2, focus);
+//			tx_col += 1;
+//			/// VELOCITY
+//			if (g_editor->pattern_view_velo) {
+//				//focus = focus_line & (g_editor->pattern_cell == 2);
+//				//if (note->mode == PATTERN_NOTE_NEW) {
+//				//	itoaw(str, note->velocity, 2);
+//				//} else {
+//				//	str[0] = '.';
+//				//	str[1] = '.';
+//				//	str[2] = 0;
+//				//}
+//				//ptext(args, p, tx_col, j, str, 5, focus);
+//				tx_col += 2;
+//			}
+//			/// PANNING
+//			if (g_editor->pattern_view_pan) {
+//				//focus = focus_line & (g_editor->pattern_cell == 3);
+//				//if (note->mode == PATTERN_NOTE_NEW) {
+//				//	itoaw(str, note->panning, 2);
+//				//} else {
+//				//	str[0] = '.';
+//				//	str[1] = '.';
+//				//	str[2] = 0;
+//				//}
+//				//ptext(args, p, tx_col, j, str, 6, focus);
+//				tx_col += 2;
+//			}
+//			/// SYNTH
+//			//focus = focus_line & (g_editor->pattern_cell == 4);
+//			//if (note->mode == PATTERN_NOTE_NEW) {
+//			//	itoaw(str, note->synth, 2);
+//			//} else {
+//			//	str[0] = '.';
+//			//	str[1] = '.';
+//			//	str[2] = 0;
+//			//}
+//			//ptext(args, p, tx_col, j, str, 4, focus);
+//			tx_col += 2;
+//			/// DELAY
+//			if (g_editor->pattern_view_glide) {
+//				//focus = focus_line & (g_editor->pattern_cell == 5);
+//				//if (note->mode != PATTERN_NOTE_KEEP) {
+//				//	itoaw(str, note->delay, 2);
+//				//} else {
+//				//	str[0] = '.';
+//				//	str[1] = '.';
+//				//	str[2] = 0;
+//				//}
+//				//ptext(args, p, tx_col, j, str, 10, focus);
+//				tx_col += 2;
+//			}
+//			/// GLIDE
+//			if (g_editor->pattern_view_delay) {
+//				//focus = focus_line & (g_editor->pattern_cell == 6);
+//				//if (note->mode == PATTERN_NOTE_GLIDE) {
+//				//	itoaw(str, note->glide, 2);
+//				//} else {
+//				//	str[0] = '.';
+//				//	str[1] = '.';
+//				//	str[2] = 0;
+//				//}
+//				//ptext(args, p, tx_col, j, str, 11, focus);
+//				tx_col += 2;
+//			}
+//			/// EFFECTS
+//			if (g_editor->pattern_view_fx) {
+//				for (k = 0; k < note_col->fx_count; ++k) {
+//					effect = &(note->effects[k]);
+//					//focus_fx = focus_line & ((g_editor->pattern_cell - 7) / 2 == k);
+//					///// COMPUTE STRINGS
+//					//if (note->mode == PATTERN_NOTE_KEEP
+//					//|| note->mode == PATTERN_NOTE_STOP
+//					//|| effect->type == PATTERN_EFFECT_NONE) {
+//					//	str[0] = '.';
+//					//	str[1] = 0;
+//					//	str[2] = '.';
+//					//	str[3] = '.';
+//					//	str[4] = 0;
+//					//} else {
+//					//	str[0] = table_effect[effect->type - 1];
+//					//	str[1] = 0;
+//					//	itoaw(str + 2, note->effects[k].value, 2);
+//					//}
+//					///// EFFECT TYPE
+//					//focus = focus_fx & ((g_editor->pattern_cell - 7) % 2 == 0);
+//					//ptext(args, p, tx_col, j, str, 13, focus);
+//					tx_col += 1;
+//					/// EFFECT VALUE
+//					//focus = focus_fx & ((g_editor->pattern_cell - 7) % 2 == 1);
+//					//ptext(args, p, tx_col, j, str + 2, 14, focus);
+//					tx_col += 2;
+//				}
+//			}
+//		}
+//		/// OFFSET X
+//		tx += (2 + 1														// PITCH
+//		/**/ + g_editor->pattern_view_velo * 2							// VELOCITY
+//		/**/ + g_editor->pattern_view_pan * 2							// PANNING
+//		/**/ + 2					  										// SYNTH
+//		/**/ + g_editor->pattern_view_glide * 2							// DELAY
+//		/**/ + g_editor->pattern_view_delay * 2							// GLIDE
+//		/**/ + g_editor->pattern_view_fx * 3 * note_col->fx_count	// EFFECTS
+//		/**/ + 1);
+//	}
+//	/// FOR EACH CV COL
+//	for (i = 0; i < pattern->cv_count; ++i) {
+//		cv_col = pattern->cvs[i];
+//		//focus_col = (g_editor->pattern_col == pattern->note_count + i);
+//		/// FOR EACH CV COL LINE
+//		for (j = 0; j < CHAR_COUNT_Y; ++j) {
+//			line = g_editor->pattern_cam_y + j;
+//			if (line >= pattern->line_count)
+//				break;
+//			//focus_line = focus_col & (g_editor->pattern_line == line);
+//			//tx_col = tx;
+//			//cv = &(cv_col->lines[line]);
+//			///// VALUE
+//			//focus = focus_line & (g_editor->pattern_cell == 0);
+//			//nvgFillColor(args.vg, colors[3]);
+//			//if (cv->mode == PATTERN_CV_KEEP) {
+//			//	str[0] = '.';
+//			//	str[1] = '.';
+//			//	str[2] = '.';
+//			//	str[3] = 0;
+//			//} else {
+//			//	itoaw(str, cv->value, 3);
+//			//}
+//			//ptext(args, p, tx_col, j, str, 3, focus);
+//			tx_col += 3;
+//			/// GLIDE
+//			//focus = focus_line & (g_editor->pattern_cell == 1);
+//			//nvgFillColor(args.vg, colors[5]);
+//			//if (cv->mode == PATTERN_CV_KEEP) {
+//			//	str[0] = '.';
+//			//	str[1] = '.';
+//			//	str[2] = 0;
+//			//} else {
+//			//	itoaw(str, cv->glide, 2);
+//			//}
+//			//ptext(args, p, tx_col, j, str, 5, focus);
+//			tx_col += 2;
+//			/// DELAY
+//			//focus = focus_line & (g_editor->pattern_cell == 2);
+//			//nvgFillColor(args.vg, colors[10]);
+//			//if (cv->mode == PATTERN_CV_KEEP) {
+//			//	str[0] = '.';
+//			//	str[1] = '.';
+//			//	str[2] = 0;
+//			//} else {
+//			//	itoaw(str, cv->delay, 2);
+//			//}
+//			//ptext(args, p, tx_col, j, str, 10, focus);
+//			tx_col += 2;
+//		}
+//		tx += (3 + 2 + 2 + 1);
+//	}
+//}
+
+static void visual_cv(
+	const Widget::DrawArgs&	args,
+	PatternSource			*pattern,
+	PatternCVCol			*cv_col,
+	Vec						p,
+	float					x) {
+	PatternCV				*cv_from;
+	PatternCV				*cv_to;
+	float					sx, sy;
+	int						line, line_from;
+	bool					first_point;
+	int						i;
+
+	/// [1] OFFSET COORD WITH CAMERA
+	x -= g_editor->pattern_cam_x;
+	if (x < 0)
+		return;
+	if (x > CHAR_COUNT_X - 1)
+		return;
+	/// [2] COMPUTE REAL COORD
+	sx = p.x + 2.0 + CHAR_W * (x + 3.0);
+	sy = p.y + 11.0 + 2.0;
+	/// [3] DRAW CURVE
+
+
+	/// FIND FIRST VISIBLE CV
+	line = g_editor->pattern_cam_y - 1;
+	while (line >= 0) {
+		if (cv_col->lines[line].mode == PATTERN_CV_SET)
+			break;
+		line -= 1;
+	}
+	if (line >= 0 && cv_col->lines[line].mode == PATTERN_CV_SET) {
+		cv_from = &(cv_col->lines[line]);
+		line_from = line;
+	} else {
+		cv_from = NULL;
+		line_from = 0;
+	}
+	/// START CURVE
+	nvgBeginPath(args.vg);
+	nvgFillColor(args.vg, colors[15]);
+	/// DRAW CURVE
+	cv_to = NULL;
+	first_point = true;
+	for (i = 0; i < CHAR_COUNT_Y; ++i) {
+		line = g_editor->pattern_cam_y + i;
+		if (line >= pattern->line_count)
+			break;
+		/// FIND NEXT VISIBLE CV
+		if (cv_col->lines[line].mode == PATTERN_CV_SET) {
+			cv_to = &(cv_col->lines[line]);
+			/// WITH PREVIOUS CV
+			if (first_point) {
+				first_point = false;
+				if (cv_from != NULL) {
+					nvgMoveTo(args.vg,
+					/**/ sx,
+					/**/ sy + CHAR_H * (line_from - 1 - g_editor->pattern_cam_y)
+					/**/ + (cv_from->delay / 99.0) * CHAR_H);
+					nvgLineTo(args.vg,
+					/**/ sx + (cv_from->value / 1000.0) * CHAR_W * 7.0,
+					/**/ sy + CHAR_H * (line_from - 1 - g_editor->pattern_cam_y)
+					/**/ + (cv_from->delay / 99.0) * CHAR_H);
+				/// WITHOUT PREVIOUS CV
+				} else {
+					nvgMoveTo(args.vg, sx, sy);
+					nvgLineTo(args.vg,
+					/**/ sx + (cv_to->value / 1000.0) * CHAR_W * 7.0,
+					/**/ sy);
+				}
+			}
+			/// DRAW CURVE
+			nvgLineTo(args.vg,
+			/**/ sx + (cv_to->value / 1000.0) * CHAR_W * 7.0,
+			/**/ sy + CHAR_H * (line - 1 - g_editor->pattern_cam_y)
+			/**/ + (cv_to->delay / 99.0) * CHAR_H);
+			/// SWAP CURVE POINTS
+			cv_from = cv_to;
+		}
+	}
+	/// END CURVE
+	if (cv_to != NULL) {
+		if (cv_from != NULL) {
+			nvgLineTo(args.vg,
+			/**/ sx + (cv_to->value / 1000.0) * CHAR_W * 7.0,
+			/**/ sy + CHAR_H * (line - 1 - g_editor->pattern_cam_y));
+			nvgLineTo(args.vg,
+			/**/ sx,
+			/**/ sy + CHAR_H * (line - 1 - g_editor->pattern_cam_y));
+		} else {
+			// ...
+		}
+	}
+	nvgFill(args.vg);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 /// PUBLIC FUNCTIONS
 ////////////////////////////////////////////////////////////////////////////////
@@ -252,6 +566,9 @@ void TrackerDisplay::draw_pattern(const DrawArgs &args, Rect rect) {
 	for (i = 0; i < pattern->cv_count; ++i) {
 		cv_col = pattern->cvs[i];
 		focus_col = (g_editor->pattern_col == pattern->note_count + i);
+
+		visual_cv(args, pattern, cv_col, p, tx);
+
 		/// FOR EACH CV COL LINE
 		for (j = 0; j < CHAR_COUNT_Y; ++j) {
 			line = g_editor->pattern_cam_y + j;
